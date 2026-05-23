7ce5ce39f0e4930d5f3ee341a1fd67c0085e7117
Chanetics <138884451+Chanetics@users.noreply.github.com>
2026-05-23 20:25:52 +0800

    fix db connection

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "kodama.proxy.rlwy.net",
   user: "root",
-  password: "",
-  database: "flower_shop"
+  password: "KpIQOiLnVpCSnDSZbstGyRBfIpDWOsYM",
+  database: "railway",
+  port: 3306
 });
 
 connection.connect(err => {
